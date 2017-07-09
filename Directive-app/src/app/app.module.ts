c181f562b05482f555bdcf04bce4c3914f44a651
Rajendhar <rajendhar.mamidala@capgemini.com>
2017-07-09 16:22:25 +0530

    Angular2 - Directives example 2

--- Directive-app/src/app/app.module.ts
+++ Directive-app/src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EmployeeComponent } from './emp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [EmployeeComponent]
 })
 export class AppModule { }
